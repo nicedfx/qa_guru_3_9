23c96ebe9e2f09e125469ee7cda5792e5a45ecc7
svasenkov <aanher@gmail.com>
2020-12-07 21:58:28 +0300

    added TestBase

--- src/test/java/tests/StudentRegistrationFormTests.java
+++ src/test/java/tests/StudentRegistrationFormTests.java
@@ -25,32 +25,7 @@ import static com.codeborne.selenide.logevents.SelenideLogger.addListener;
 import static helpers.AttachmentsHelper.*;
 import static io.qameta.allure.Allure.step;
 
-public class StudentRegistrationFormTests {
-
-    @BeforeAll
-    static void setup() {
-        addListener("AllureSelenide", new AllureSelenide().screenshots(true).savePageSource(true));
-
-        DesiredCapabilities capabilities = new DesiredCapabilities();
-        capabilities.setCapability("enableVNC", true);
-        capabilities.setCapability("enableVideo", true);
-
-        Configuration.browserCapabilities = capabilities;
-        Configuration.remote = "https://user1:1234@" + System.getProperty("remote.browser.url") + ":4444/wd/hub/";
-        Configuration.startMaximized = true;
-        //        Configuration.browser = FIREFOX;
-    }
-
-    @AfterEach
-    @Step("Attachments")
-    public void afterEach(){
-        attachScreenshot("Last screenshot");
-        attachPageSource();
-        attachAsText("Browser console logs", getConsoleLogs());
-        attachVideo();
-
-        closeWebDriver();
-    }
+public class StudentRegistrationFormTests extends TestBase {
 
     @Test
     @DisplayName("Successful fill registration form")
