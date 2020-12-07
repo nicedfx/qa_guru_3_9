166c72d6510c78d96493fa070b836ad082508eb6
svasenkov <aanher@gmail.com>
2020-12-07 20:30:46 +0300

    added allure steps

--- src/test/java/StudentRegistrationFormTests.java
+++ src/test/java/StudentRegistrationFormTests.java
@@ -12,6 +12,7 @@ import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Condition.visible;
 import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.*;
+import static io.qameta.allure.Allure.step;
 
 public class StudentRegistrationFormTests {
 
@@ -46,50 +47,56 @@ public class StudentRegistrationFormTests {
                 state = "Uttar Pradesh",
                 city = "Merrut";
 
-        open("https://demoqa.com/automation-practice-form");
-        $(".practice-form-wrapper").shouldHave(text("Student Registration Form"));
+        step("Open students registration form", () -> {
+            open("https://demoqa.com/automation-practice-form");
+            $(".practice-form-wrapper").shouldHave(text("Student Registration Form"));
+        });
 
-        $("#firstName").val(firstName);
-        $("#lastName").val(lastName);
-        $("#userEmail").val(userEmail);
-        $("#genterWrapper").$(byText(gender)).click();
-        $("#userNumber").val(userNumber);
-        // set date
-        $("#dateOfBirthInput").click();
-        $(".react-datepicker__month-select").selectOption(monthOfBirth);
-        $(".react-datepicker__year-select").selectOption(yearOfBirth);
-        $(".react-datepicker__day--0" + dayOfBirth).click();
-        // set subject
-        $("#subjectsInput").val(subject1Prefix);
-        $(".subjects-auto-complete__menu-list").$(byText(subject1)).click();
-        $("#subjectsInput").val(subject2Prefix);
-        $(".subjects-auto-complete__menu-list").$(byText(subject2)).click();
-        // set hobbies
-        $("#hobbiesWrapper").$(byText(hobby1)).click();
-        $("#hobbiesWrapper").$(byText(hobby2)).click();
-        $("#hobbiesWrapper").$(byText(hobby3)).click();
+        step("Fill students registration form", () -> {
+            $("#firstName").val(firstName);
+            $("#lastName").val(lastName);
+            $("#userEmail").val(userEmail);
+            $("#genterWrapper").$(byText(gender)).click();
+            $("#userNumber").val(userNumber);
+            // set date
+            $("#dateOfBirthInput").click();
+            $(".react-datepicker__month-select").selectOption(monthOfBirth);
+            $(".react-datepicker__year-select").selectOption(yearOfBirth);
+            $(".react-datepicker__day--0" + dayOfBirth).click();
+            // set subject
+            $("#subjectsInput").val(subject1Prefix);
+            $(".subjects-auto-complete__menu-list").$(byText(subject1)).click();
+            $("#subjectsInput").val(subject2Prefix);
+            $(".subjects-auto-complete__menu-list").$(byText(subject2)).click();
+            // set hobbies
+            $("#hobbiesWrapper").$(byText(hobby1)).click();
+            $("#hobbiesWrapper").$(byText(hobby2)).click();
+            $("#hobbiesWrapper").$(byText(hobby3)).click();
+            $("#uploadPicture").uploadFile(new File("src/test/resources/" + picture));
+            $("#currentAddress").val(currentAddress);
+            // set state and city
+            $("#state").click();
+            $("#stateCity-wrapper").$(byText(state)).click();
+            $("#city").click();
+            $("#stateCity-wrapper").$(byText(city)).click();
 
-        $("#uploadPicture").uploadFile(new File("src/test/resources/" + picture));
-        // set state and city
-        $("#state").click();
-        $("#stateCity-wrapper").$(byText(state)).click();
-        $("#city").click();
-        $("#stateCity-wrapper").$(byText(city)).click();
+            $("#submit").click();
+        });
 
-        $("#submit").click();
+        step("Verify successful form submit", () -> {
+            $("#example-modal-sizes-title-lg").shouldHave(text("Thanks for submitting the form"));
+            $x("//td[text()='Student Name']").parent().shouldHave(text(firstName + " " + lastName));
+            $x("//td[text()='Student Email']").parent().shouldHave(text(userEmail));
+            $x("//td[text()='Gender']").parent().shouldHave(text(gender));
+            $x("//td[text()='Mobile']").parent().shouldHave(text(userNumber));
+            $x("//td[text()='Date of Birth']").parent().shouldHave(text(dayOfBirth + " " + monthOfBirth + "," + yearOfBirth));
+            $x("//td[text()='Subjects']").parent().shouldHave(text(subject1 + ", " + subject2));
+            $x("//td[text()='Hobbies']").parent().shouldHave(text(hobby1 + ", " + hobby2 + ", " + hobby3));
+            $x("//td[text()='Picture']").parent().shouldHave(text(picture));
+            $x("//td[text()='Address']").parent().shouldHave(text(currentAddress));
+            $x("//td[text()='State and City']").parent().shouldHave(text(state + " " + city));
+        });
 
-        // asserts
-        $("#example-modal-sizes-title-lg").shouldHave(text("Thanks for submitting the form"));
-        $x("//td[text()='Student Name']").parent().shouldHave(text(firstName + " " + lastName));
-        $x("//td[text()='Student Email']").parent().shouldHave(text(userEmail));
-        $x("//td[text()='Gender']").parent().shouldHave(text(gender));
-        $x("//td[text()='Mobile']").parent().shouldHave(text(userNumber));
-        $x("//td[text()='Date of Birth']").parent().shouldHave(text(dayOfBirth + " " + monthOfBirth + "," + yearOfBirth));
-        $x("//td[text()='Subjects']").parent().shouldHave(text(subject1 + ", " + subject2));
-        $x("//td[text()='Hobbies']").parent().shouldHave(text(hobby1 + ", " + hobby2 + ", " + hobby3));
-        $x("//td[text()='Picture']").parent().shouldHave(text(picture));
-        $x("//td[text()='Address']").parent().shouldHave(text(currentAddress));
-        $x("//td[text()='State and City']").parent().shouldHave(text(state + " " + city));
     }
 
 }
